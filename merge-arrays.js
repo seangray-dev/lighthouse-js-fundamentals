bffb9274690326df21dfd6c0ac0c753188745076
Sean Gray <seangray.dev@gmail.com>
2023-01-01 16:14:52 +0000

    merge sorted array

---
+++ merge-arrays.js
@@ -0,0 +1,9 @@
+const merge = (arr1, arr2) => {
+  const mergeArray = arr1.concat(arr2);
+  mergeArray.sort();
+  return mergeArray;
+};
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]), "=?", [1, 2, 3, 4, 4, 5, 6]);
+console.log(merge([4], [2, 5, 8]), "=?", [2, 4, 5, 8]);
+console.log(merge([1, 2, 6], []), "=?", [1, 2, 6]);
