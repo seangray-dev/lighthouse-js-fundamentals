3736308b73e2d1413247e53c1145327caee5b6bb
Sean Gray <seangray.dev@gmail.com>
2023-01-01 16:07:45 +0000

    concate arrays

---
+++ concatenate-arrays.js
@@ -0,0 +1,9 @@
+function concat(a, b) {
+  const array = a.concat(b);
+  return array;
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
+console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+console.log(concat([5, 10], []), "=?", [5, 10]);
